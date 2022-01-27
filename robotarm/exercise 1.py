689628357690de5016b395dd299fc24b6c038d13
obaid <obaidaqurea321@gmail.com>
2022-01-27 20:38:53 +0100

    (exer 2 )

---
+++ robotarm/exercise 1.py
@@ -0,0 +1,20 @@
+from RobotArm import RobotArm
+            
+             # De robotarm opstarten in een bepaald level, bijvoorbeeld 'exercise 1'
+robotArm = RobotArm('exercise 1')
+
+robotArm.moveRight()
+
+robotArm.grab()   
+robotArm.moveLeft ()
+robotArm.drop()                       # Jouw python instructies zet je vanaf hier:
+
+             # Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
+
+from RobotArm import RobotArm
+            
+             # De robotarm opstarten in een bepaald level, bijvoorbeeld 'exercise 1'
+robotArm = RobotArm('exercise 1')
+
+robotArm.wait()
